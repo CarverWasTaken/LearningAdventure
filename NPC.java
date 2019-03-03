dffb65a53fa8bc87b58789eb050d47af9860c0f5
CarverWasTaken <40581977+CarverWasTaken@users.noreply.github.com>
2019-03-03 12:30:52 -0500

    NPC Update
    Finalized NPC Dialogue and fixed bug where JTextArea would compete with Main.window for focus

--- NPC.java
+++ NPC.java
@@ -1,31 +1,53 @@
 import java.awt.Color;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class NPC extends JPanel{
 	private int x = 0, y =0; 
 	private String dialogue;
-	JPanel dialoguePanel = new JPanel();
+	JLabel dialoguePanel = new JLabel();
+	JTextArea dialogueArea;
 	
 	 NPC(int x1, String s){
 		x = x1;
 		y = AdventureManager.floorHeight-100;
+		dialogue = s;
 		setBackground(Color.blue);
 		setSize(50,100);
 		setLocation(x,y);
 		setVisible(true);
 		AdventureManager.mainPanel.repaint();
-		 
+		makePanel();
 	}
-	public void showDialogue(){
-		 
+	 public void makePanel() {
 		 dialoguePanel.setSize(300,300);
+		 dialoguePanel.setIcon(new ImageIcon(getClass().getClassLoader().getResource("textBubble.png")));
 		 dialoguePanel.setLocation(x-130, y-300);
 		 dialoguePanel.setBackground(Color.green);
 		 dialoguePanel.setLayout(null);
 		 AdventureManager.mainPanel.add(dialoguePanel);
 		 dialoguePanel.setVisible(true);
+		 
+		 dialogueArea = new JTextArea();
+		 dialogueArea.setEditable(false);
+		 dialogueArea.setLocation(8,5);
+		 dialogueArea.setOpaque(false);
+		 dialogueArea.setSize(290,200);
+		 dialogueArea.setLayout(null);
+		 dialogueArea.setForeground(Color.black);
+		 dialogueArea.setBackground(Color.red);
+		 dialogueArea.setText(dialogue);
+		 dialoguePanel.add(dialogueArea);
+		 dialogueArea.setVisible(true);
+	 }
+	public void showDialogue(){
+		 
+		 dialoguePanel.setVisible(true);
+		 Main.window.requestFocus();
 		AdventureManager.mainPanel.repaint();
 	 }
 	public void hideDialogue(){
@@ -36,7 +58,7 @@ public class NPC extends JPanel{
 				|| ((AdventureManager.toon.x > x) && (AdventureManager.toon.x < x+50)&&(AdventureManager.toon.y+98> y) &&(AdventureManager.toon.y+98< y+100) )
 					
 					) {
-				showDialogue();
+				showDialogue(); 
 			}
 		else {
 			hideDialogue();
